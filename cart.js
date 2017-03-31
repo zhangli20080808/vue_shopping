949a39851b13d2b5b37f28ef97891110639ab208
力力娃 <liliwa@liliwadeMacBook-Pro.local>
2017-03-31 14:44:30 +0800

    一个标准的vm；

---
+++ cart.js
@@ -0,0 +1,21 @@
+/**
+ * Created by liliwa on 17/3/31.
+ */
+new Vue({
+    el:"#app",
+    data:{
+        message:'hello world'
+    },
+    //过滤器
+    filter:{
+
+    },
+    //1.0我们使用ready  2我们使用mounted 实例化完成后默认查询某个方法
+    mounted:{
+
+    },
+    methods:{
+    //    事件的绑定
+
+    }
+});
